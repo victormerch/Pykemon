ad56c7c5b96ee34ef3594ecf1b17eaeef0d3cc57
victormerch <1Victormerch@gmail.com>
2022-10-06 19:51:19 +0200

    Codigo sucio, panatlla2 acabada y pantalla3 en proceso

--- app.py
+++ app.py
@@ -1,5 +1,38 @@
 # Importar e inicializar Pygame.
+import re
 import pygame
+import random
+
+class Pokemon:
+    # Constructor de la clase Pokemon.
+    def __init__(self, name, type, imgFront, imgBack):
+        self.name = name
+        self.type = type
+        self.imgFront = imgFront
+        self.imgBack = imgBack
+    
+    #Getters
+    def getType(self):
+        return self.type
+    def getImgFront(self):
+        return self.imgFront
+    def getImgBack(self):
+        return self.imgBack
+    #Setters
+    def setType(self, type):
+        self.type = type
+    def setImgFront(self, imgFront):
+        self.imgFront = imgFront
+    def setImBack(self, imgBack):
+        self.imgBack = imgBack
+        
+    def __str__(self):
+        return self.name
+
+pokemons = [Pokemon("Charmander", "Fuego", "Images/Sprites/4_charmander_front.png", "Images/Sprites/4_charmander_back.png"),
+             Pokemon("Squirtle", "Agua", "Images/Sprites/7_squirtle_front.png", "Images/Sprites/7_squirtle_back.png"),
+             Pokemon("Bulbasaur", "Planta", "Images/Sprites/1_bulbasaur_front.png", "Images/Sprites/1_bulbasaur_back.png")]
+    
 pygame.init()
 
 # Crear la ventana y poner el tamaño.
@@ -10,6 +43,10 @@ bg = pygame.image.load("Images/bg1.png")
 #Background
 pygame.display.set_caption("Pykemon")
 screen.blit(bg, (0, 0))
+#Texto de escoje tu pokemon
+font = pygame.font.Font('Fonts/pokemon_generation_1.ttf', 12)
+text = font.render('Pulsa para emepzar...', True,(250,250,250))
+screen.blit(text,(220,580))
 #Icon of the game
 programIcon = pygame.image.load('Images/icon.png')
 pygame.display.set_icon(programIcon)
@@ -21,6 +58,8 @@ blue = (0, 0, 128)
 
 clock = pygame.time.Clock()
 salir = False
+pantalla2 = False
+pantalla3 = False
 
 # Loop principal (game loop) del juego.
 while not salir:
@@ -40,15 +79,65 @@ while not salir:
             if event.key == pygame.K_ESCAPE:
                 salir = True
             else:
-                #Cambiamos el color de fondo
-                screen.fill([235, 240, 186])
-                #Añadimos los pokemons
-                pokemon1 = pygame.image.load("Images/Sprites/4_charmander_front.png").convert_alpha()
-                pokemon2 = pygame.image.load("Images/Sprites/7_squirtle_front.png").convert_alpha()
-                pokemon3 = pygame.image.load("Images/Sprites/1_bulbasaur_front.png").convert_alpha()
-                screen.blit(pygame.transform.scale(pokemon1, (100, 100)), (60, 320))
-                screen.blit(pygame.transform.scale(pokemon2, (100, 100)), (250, 320))
-                screen.blit(pygame.transform.scale(pokemon3, (100, 100)), (147, 460))
+                pantalla2 = True
+                
+        if pantalla2: 
+            
+            #PANEL DE ELECION DE POKEMON
+            #Cambiamos el color de fondo
+            screen.fill([235, 240, 186])
+            #Añadimos los pokemons
+            pokemon1 = pygame.image.load(pokemons[0].getImgFront()).convert_alpha()
+            pokemon2 = pygame.image.load(pokemons[1].getImgFront()).convert_alpha()
+            pokemon3 = pygame.image.load(pokemons[2].getImgFront()).convert_alpha()
+            screen.blit(pygame.transform.scale(pokemon1, (100, 100)), (60, 320))
+            screen.blit(pygame.transform.scale(pokemon2, (100, 100)), (250, 320))
+            screen.blit(pygame.transform.scale(pokemon3, (100, 100)), (147, 460))
+            #Texto de escoje tu pokemon
+            text = font.render('Escoje tu Pokemon ...', True,(0,0,0))
+            font = pygame.font.Font('Fonts/pokemon_generation_1.ttf', 18)
+            screen.blit(text,(65,180))   
+            x, y = pygame.mouse.get_pos()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed()[0]:
+                    if pygame.Rect(60, 320, 100, 100).collidepoint(x, y):
+                        pantalla2 = False
+                        pantalla3 = True
+                        
+                        pokemonUser = pokemons[0]
+                        pokemonBot = random.choice(pokemons)
+                        bgs = ["background_1.png","background_2.png","background_3.png","background_4.png"]
+                        bgEscenario = random.choice(bgs)
+                    elif pygame.Rect(250, 320, 100, 100).collidepoint(x, y):
+                        pantalla2 = False
+                        pantalla3 = True
+                        
+                        pokemonUser = pokemons[1]
+                        pokemonBot = random.choice(pokemons)
+                        bgs = ["background_1.png","background_2.png","background_3.png","background_4.png"]
+                        bgEscenario = random.choice(bgs)
+                    elif pygame.Rect(147, 460, 100, 100).collidepoint(x, y):
+                        pantalla2 = False
+                        pantalla3 = True
+                        
+                        pokemonUser = pokemons[2]
+                        pokemonBot = random.choice(pokemons)
+                        bgs = ["background_1.png","background_2.png","background_3.png","background_4.png"]
+                        bgEscenario = random.choice(bgs)
+        if pantalla3:   
+            #PANEL DE BATALLA
+            screen.fill([235, 240, 186])
+            
+            #Escenario
+            escenario = pygame.image.load("Images/Backgrounds/"+bgEscenario).convert_alpha()
+            screen.blit(pygame.transform.scale(escenario, (350, 250)), (25, 20))
+            #pokemons
+            pokemon1 = pygame.image.load(pokemonUser.getImgBack()).convert_alpha()
+            screen.blit(pygame.transform.scale(pokemon1, (100, 100)), (30, 160))
+            
+            pokemon2 = pygame.image.load(pokemonBot.getImgFront()).convert_alpha()
+            screen.blit(pygame.transform.scale(pokemon2, (100, 100)), (230, 70))
+        
                 
                 
     # Actualizar la pantalla.
---
+++ pruebas.py
@@ -0,0 +1,60 @@
+import pygame
+ 
+pygame.init()
+screen = pygame.display.set_mode((500, 600))
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Arial", 20)
+ 
+ 
+class Button:
+    """Create a button, then blit the surface in the while loop"""
+ 
+    def __init__(self, text,  pos, font, bg="black", feedback=""):
+        self.x, self.y = pos
+        self.font = pygame.font.SysFont("Arial", font)
+        if feedback == "":
+            self.feedback = "text"
+        else:
+            self.feedback = feedback
+        self.change_text(text, bg)
+ 
+    def change_text(self, text, bg="black"):
+        """Change the text whe you click"""
+        self.text = self.font.render(text, 1, pygame.Color("White"))
+        self.size = self.text.get_size()
+        self.surface = pygame.Surface(self.size)
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (0, 0))
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+ 
+    def show(self):
+        screen.blit(button1.surface, (self.x, self.y))
+ 
+    def click(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.rect.collidepoint(x, y):
+                    self.change_text(self.feedback, bg="red")
+ 
+ 
+def mainloop():
+    """ The infinite loop where things happen """
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            button1.click(event)
+        button1.show()
+        clock.tick(30)
+        pygame.display.update()
+ 
+ 
+button1 = Button(
+    "Click here",
+    (100, 100),
+    font=30,
+    bg="navy",
+    feedback="You clicked me")
+ 
+mainloop()
